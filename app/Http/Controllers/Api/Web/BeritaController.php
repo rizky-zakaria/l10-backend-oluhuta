867439c7cef9bf2d2f5cc49ba7e36bd9344fd379
Rizky Zakaria <rizky.zakaria78@gmail.com>
2023-11-19 21:54:47 +0700

    --allow-empty

--- app/Http/Controllers/Api/Web/BeritaController.php
+++ app/Http/Controllers/Api/Web/BeritaController.php
@@ -5,16 +5,19 @@ namespace App\Http\Controllers\Api\Web;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\BeritaResource;
 use App\Models\Berita;
+use App\Models\Konten;
 use Illuminate\Http\Request;
 
 class BeritaController extends Controller
 {
     public function index()
     {
-        $data = Berita::join('gambars', 'gambars.id', '=', 'beritas.gambar_id')
-            ->when(request()->q, function ($data) {
-                $data = $data->where('beritas.judul', 'like', '%' . request()->q . '%');
-            })->latest()->paginate(3);
+        $data = Konten::select(['kontens.*', 'gambars.path', 'gambars.jenis'])
+            ->join('gambars', 'gambars.id', '=', 'kontens.gambar_id')
+            ->where('kontens.kategori_id', 4)
+            // ->get(['kontens.*', 'gambars.path', 'gambars.jenis'])
+            ->latest()
+            ->paginate(3);
 
         return new BeritaResource(true, 'List Data Berita', $data);
     }
